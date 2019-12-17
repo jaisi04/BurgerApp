a4c79a47daf73ad2be85f8c6ba9480b2220ee078
jaisi04 <akshayjaiswal0004@gmail.com>
2019-12-17 23:53:41 +0530

    updating firebase endpoint

--- app/src/axios-orders.js
+++ app/src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL : 'https://react-burger-builder-9eb5f.firebaseio.com/'
+    baseURL : 'https://react-bbuilder-37679.firebaseio.com/'
 });
 
 export default instance;
